8136c11fe019458224ea8502ea9450a847d1139e
fernandezpaulo0724 <fernandezpaulo0724@gmail.com>
2021-08-23 23:25:07 +0800

    modified activity code

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+
+
 function printUserInfo() {
   
-let name = prompt('Name: ', "tony starks");
-let email = prompt('Email: ', "ironman@gmail.com");
-let number = prompt('Number: ', "0917111111");
+var name = prompt('Name: ', "tony starks");
+var email = prompt('Email: ', "ironman@gmail.com");
+var number = prompt('Number: ', "0917111111");
 
-  	
+if (name != null) {
+  document.getElementById("demo1").innerHTML =
+  "Name: " + name ;
+}
+
+if (email != null) {
+  document.getElementById("demo2").innerHTML =
+  "Email: " + email ;
+}
+
+if (number != null) {
+  document.getElementById("demo3").innerHTML =
+  "Number: " + number ;
+}
   
 }
 
