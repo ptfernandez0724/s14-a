1534116946d15e2ddfb616ee9e9da159d2418bd6
fernandezpaulo0724 <fernandezpaulo0724@gmail.com>
2021-08-23 21:39:17 +0800

    added activity code

---
+++ script.js
@@ -0,0 +1,14 @@
+function printUserInfo() {
+  
+let name = prompt('Name: ', "tony starks");
+let email = prompt('Email: ', "ironman@gmail.com");
+let number = prompt('Number: ', "0917111111");
+
+  	
+  
+}
+
+  printUserInfo();
+ 
+
+
